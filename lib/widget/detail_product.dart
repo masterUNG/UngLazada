d6ad5266155c4357433cdf54a30fc6e05f366eab
masterUNG <phrombutr@gmail.com>
2020-02-25 16:02:08 +0700

    finish 25feb

--- lib/widget/detail_product.dart
+++ lib/widget/detail_product.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
 
-
 class DetailProduct extends StatefulWidget {
+  final String name, detail, pathUrl;
+  DetailProduct({Key key, this.name, this.detail, this.pathUrl})
+      : super(key: key);
+
   @override
   _DetailProductState createState() => _DetailProductState();
 }
 
 class _DetailProductState extends State<DetailProduct> {
+  // Field
+  String name = '', detail, pathUrl;
+
+  // Method
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    name = widget.name;
+    detail = widget.detail;
+    pathUrl = widget.pathUrl;
+  }
+
+  Widget showImage() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.5,
+      child: Image.network(pathUrl),
+    );
+  }
+
+  Widget showText(){
+    return Text(detail);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Detail '),),
+      appBar: AppBar(
+        title: Text('Detail $name'),
+      ),
+      body: ListView(
+        children: <Widget>[showImage(),showText(),],
+      ),
     );
   }
 }
--- lib/widget/list_product.dart
+++ lib/widget/list_product.dart
@@ -1,4 +1,7 @@
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:unglazada/widget/detail_product.dart';
 
 class ListProduct extends StatefulWidget {
   final String category;
@@ -9,9 +12,12 @@ class ListProduct extends StatefulWidget {
 }
 
 class _ListProductState extends State<ListProduct> {
-
   // Field
   String currentCategory = '';
+  List<String> names = List();
+  List<String> details = List();
+  List<String> pictures = List();
+  List<String> codes = List();
 
   // Method
   @override
@@ -19,15 +25,126 @@ class _ListProductState extends State<ListProduct> {
     // TODO: implement initState
     super.initState();
     currentCategory = widget.category;
+    readFirestore();
   }
 
+  Future<void> readFirestore() async {
+    Firestore firestore = Firestore.instance;
+    CollectionReference collectionReference =
+        firestore.collection(currentCategory);
+    await collectionReference.snapshots().listen((dataSnapshot) {
+      List<DocumentSnapshot> documentSnapshots = dataSnapshot.documents;
+      for (var snapshot in documentSnapshots) {
+        // String name = snapshot.data['Name'];
+        // print('name = $name');
+
+        setState(() {
+          String name = snapshot.data['Name'];
+          name = checkNull(name);
+
+          String detail = snapshot.data['Detail'];
+          detail = checkNull(detail);
+
+          String picture = snapshot.data['Image'];
+          picture = checkNull(picture);
+
+          String code = snapshot.data['Code'];
+          code = checkNull(code);
+
+          names.add(name);
+          details.add(detail);
+          pictures.add(picture);
+          codes.add(code);
+        });
+      }
+    });
+  }
+
+  String checkNull(String string) {
+    if (string == null) {
+      return 'No Data';
+    } else {
+      return string;
+    }
+  }
+
+  Widget showProcess() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget showPicture(int index) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: MediaQuery.of(context).size.width * 0.4,
+      padding: EdgeInsets.all(20.0),
+      child: Image.network(pictures[index]),
+    );
+  }
+
+  Widget showText(int index) {
+    return Column(
+      children: <Widget>[
+        Text(names[index]),
+        Text(details[index]),
+      ],
+    );
+  }
+
+  Widget showListView() {
+    return ListView.builder(
+      itemCount: names.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () {
+            MaterialPageRoute route =
+                MaterialPageRoute(builder: (BuildContext buildContext) {
+              return DetailProduct(
+                name: names[index],
+                detail: details[index],
+                pathUrl: pictures[index],
+              );
+            });
+            Navigator.of(context).push(route);
+          },
+          child: Row(
+            children: <Widget>[
+              showPicture(index),
+              showText(index),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget readCodeButton() {
+    return IconButton(
+      icon: Icon(Icons.monochrome_photos),
+      onPressed: () {
+        readCodeThread();
+      },
+    );
+  }
+
+  Future<void> readCodeThread()async{
+    try {
+
+      String string = await BarcodeScanner.scan();
+      print('string = $string');
+      
+    } catch (e) {
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: AppBar(actions: <Widget>[readCodeButton()],
         title: Text('List $currentCategory'),
       ),
+      body: names.length == 0 ? showProcess() : showListView(),
     );
   }
 }
